3a87e499d35b25bde3edf9cd6fd790a27d194b35
jct13761 <jct13761@uga.edu>
2020-11-27 21:15:22 -0500

    Added tester pages, fixed some CSS issues.

--- pages/footer.php
+++ pages/footer.php
@@ -1,3 +1,15 @@
-<footer>
-    <p id="copy">Copyright &copy; 2020 Hi-Rez. All rights reserved. This content is not my own nor do I make any claim to the images and information presented on the website</p>
+<footer id="footer">
+    <!-- <div id="footer" class="container text-center">
+        <p id="copy">Copyright &copy; 2020 Hi-Rez. All rights reserved.
+            This content is not my own nor do I make any claim to the images
+            and information presented on the website</p>
+    </div> -->
+    <div class="container text-center" id="con">
+        <!-- <div id="content">Results will go here</div> -->
+        <div id="footer" class="container text-center">
+            <p id="copy">Copyright &copy; 2020 Hi-Rez. All rights reserved.
+                This content is not my own nor do I make any claim to the images
+                and information presented on the website</p>
+        </div>
+    </div>
 </footer>
--- pages/home.php
+++ pages/home.php
@@ -2,7 +2,7 @@
 include('database.php');
 
 //TESTER search query
-$term = "aura";
+$term = "health";
 $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
 $results = $db->prepare($query);
 $results->execute();
@@ -12,20 +12,184 @@ $results->execute();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
     <title>Smite Item Shop</title>
     <!-- Favicon SOURCE:  https://www.smitegame.com -->
     <link rel="shortcut icon" href="../images/favicon.png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="../styles/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <?php include('header.php'); ?>
-    <div id="homepage_content">
-        <img src="../ItemImages/emperors-armor.jpg">
-        <?php foreach ($results as $result) : ?>
-            <p><?php echo $result['itemName']; ?></p>
-        <?php endforeach; ?>
+    <div class="container" id="homepage_content">
+        <div class="container-fluid" id="image_containter">
+            <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+            <?php foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+
+
+
+
+
+            <!-- REPEAT LOOP -->
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php $query = "SELECT * FROM items WHERE itemKeywords LIKE '%$term%'";
+            $results = $db->prepare($query);
+            $results->execute();
+            foreach ($results as $result) : ?>
+                <div class="col-md-2 text-center" id="item_box">
+                    <?php $id = $result['itemID'] ?>
+                    <button data-toggle="collapse" data-target="#demo" id="image_button">
+                        <!-- <img src="../ItemImages/emperors-armor.jpg"> -->
+                        <img src="../<?php echo $result['itemImage']; ?>">
+                        <p><?php echo $result['itemName']; ?></p>
+                    </button>
+                    <div id="demo" class="collapse">
+                        Lorem ipsum dolor text....
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+
+
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6" style="background-color:pink;">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.
+            </div>
+        </div>
     </div>
     <?php include('footer.php'); ?>
 </body>
