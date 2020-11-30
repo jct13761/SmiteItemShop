13b8085fb35b0f024e9f48231815838704970e38
jct13761 <jct13761@uga.edu>
2020-11-30 17:35:07 -0500

    fixed modality issue

--- pages/header.php
+++ pages/header.php
@@ -1,20 +1,23 @@
 <!-- <nav class="navbar navbar-default navbar-static-top bg-dark navbar-dark"> -->
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark header">
+    <a class="navbar-brand" href="home.php">
+        <img src="../images/Logo.png" alt="logo" style="width:40px;">
+    </a>
     <!-- <div class="container"> -->
 
-        <ul class="nav navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
+    <ul class="nav navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="Tests/test.html">Gods</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="Tests/test.html">About</a>
+        </li>
+        <!-- <li class="nav-item">
                 <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
+            </li> -->
+    </ul>
     <!-- </div> -->
 </nav>
