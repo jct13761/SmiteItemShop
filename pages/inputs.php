f679c6178fd9e712ae960de7cdb2f2e838849a84
jct13761 <jct13761@uga.edu>
2020-12-03 22:32:58 -0500

    Fixed checkbox IDs

--- pages/inputs.php
+++ pages/inputs.php
@@ -2,14 +2,12 @@
 
     <!-- Search bar and reset button -->
     <div class="row search_row">
-        <!-- <div class="col-sm-1"></div>
-        <div class="col-sm-10"> -->
-
         <!-- Search button -->
         <div class="image-container">
             <input type="search" id="search-bar" placeholder="Search Items...<?php echo "<img id='search-image' src='../images/search.svg'>" ?>">
             <img id="search-image" src="../images/search.svg">
-            <a href="home.php"><b>Products</b><img id="search-image" src="../images/search.svg"></a>
+            <!-- <a href="home.php"><b>Products</b><img id="search-image" src="../images/search.svg"></a> -->
+            <!-- Reset filter Link -->
             <a href="?">Reset Filters</a>
         </div>
     </div>
@@ -19,119 +17,118 @@
     <div class="row text-center checkbox_row">
         <div class="col-xs-12 checkbox_box" >
 
-
             <!-- Tiers -->
             <div class="col-xs-12 checkbox_col text-center">
-                <div class="checkbox_center checkbox_container" >
+                <div class="checkbox_center checkbox_container" id="tier_checkbox">
                     <h5>Tier: </h5>
                     <div class="form-check">
                         <!-- THIS LINE HAS THE "for" ATTRIBUTE: <label class="form-check-label" for="TierCheck1"> -->
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="Check1">Tier 1
+                            <input type="checkbox" class="form-check-input" value="" id="TierCheck1">Tier 1
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="Check2">Tier 2
+                            <input type="checkbox" class="form-check-input" value="" id="TierCheck2">Tier 2
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="Check3">Tier 3
+                            <input type="checkbox" class="form-check-input" value="" id="TierCheck3">Tier 3
                         </label>
                         <div class="clear"></div>
                     </div>
                 </div>
             </div>
 
-            <!-- GENERAL -->
+            <!-- Physical -->
             <div class="col-xs-12 checkbox_col text-center">
-                <div class="checkbox_center checkbox_container" id="general_checkbox">
+                <div class="checkbox_center checkbox_container" id="physical_checkbox">
                     <h5>Physical: </h5>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck1">Physical
+                            <input type="checkbox" class="form-check-input" value="" id="PhysCheck1">Physical
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck1">Physical Power
+                            <input type="checkbox" class="form-check-input" value="" id="PhysCheck2">Physical Power
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck4">Physical Lifesteal
+                            <input type="checkbox" class="form-check-input" value="" id="PhysCheck3">Physical Lifesteal
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck9">Physical Penetration
+                            <input type="checkbox" class="form-check-input" value="" id="PhysCheck4">Physical Penetration
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck10">Physical Flat Pen
+                            <input type="checkbox" class="form-check-input" value="" id="PhysCheck5">Physical Flat Pen
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck11">Physical % Pen
+                            <input type="checkbox" class="form-check-input" value="" id="PhysCheck6">Physical % Pen
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck12">Crit Chance
+                            <input type="checkbox" class="form-check-input" value="" id="PhysCheck">Crit Chance
                         </label>
                         <div class="clear"></div>
                     </div>
                 </div>
             </div>
 
-            <!-- OFFENSIVE -->
+            <!-- MAGICAL -->
             <div class="col-xs-12 checkbox_col text-center">
-                <div class="checkbox_center checkbox_container" id="offensive_checkbox">
+                <div class="checkbox_center checkbox_container" id="magical_checkbox">
                     <h5>Magical: </h5>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck2">Magical
+                            <input type="checkbox" class="form-check-input" value="" id="MagCheck1">Magical
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck2">Magical Power
+                            <input type="checkbox" class="form-check-input" value="" id="MagCheck2">Magical Power
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck5">Magical Lifesteal
+                            <input type="checkbox" class="form-check-input" value="" id="MagCheck3">Magical Lifesteal
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck9">Magical Penetration
+                            <input type="checkbox" class="form-check-input" value="" id="MagCheck4">Magical Penetration
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck10">Magical Flat Pen
+                            <input type="checkbox" class="form-check-input" value="" id="MagCheck5">Magical Flat Pen
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck11">Magical % Pen
+                            <input type="checkbox" class="form-check-input" value="" id="MagCheck6">Magical % Pen
                         </label>
                         <div class="clear"></div>
                     </div>
@@ -145,37 +142,37 @@
                     <h5>Defensive: </h5>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="DefensiveCheck1">Physical Protection
+                            <input type="checkbox" class="form-check-input" value="" id="DefCheck1">Physical Protection
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="DefensiveCheck2">Magical Protection
+                            <input type="checkbox" class="form-check-input" value="" id="DefCheck2">Magical Protection
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="DefensiveCheck3">Health
+                            <input type="checkbox" class="form-check-input" value="" id="DefCheck3">Health
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="DefensiveCheck3">CCR
+                            <input type="checkbox" class="form-check-input" value="" id="DefCheck4">CCR
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="DefensiveCheck4">HP5
+                            <input type="checkbox" class="form-check-input" value="" id="DefCheck4">HP5
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="DefensiveCheck4">Damage % reduction
+                            <input type="checkbox" class="form-check-input" value="" id="DefCheck5">Damage % reduction
                         </label>
                         <div class="clear"></div>
                     </div>
@@ -189,43 +186,43 @@
                     <h5>Utility: </h5>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="UtilityCheck1">Movement Speed
+                            <input type="checkbox" class="form-check-input" value="" id="UtilCheck1">Movement Speed
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="UtilityCheck1">Slow
+                            <input type="checkbox" class="form-check-input" value="" id="UtilCheck2">Slow
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="UtilityCheck2">Cooldown %
+                            <input type="checkbox" class="form-check-input" value="" id="UtilCheck3">Cooldown %
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="UtilityCheck3">Mana
+                            <input type="checkbox" class="form-check-input" value="" id="UtilCheck4">Mana
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="UtilityCheck4">MP5
+                            <input type="checkbox" class="form-check-input" value="" id="UtilCheck5">MP5
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OtherCheck2">Anti-Heal
+                            <input type="checkbox" class="form-check-input" value="" id="UtilCheck6">Anti-Heal
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OffCheck3">Attack Speed
+                            <input type="checkbox" class="form-check-input" value="" id="UtilCheck7">Attack Speed
                         </label>
                         <div class="clear"></div>
                     </div>
@@ -245,7 +242,7 @@
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OtherCheck10">Evolved
+                            <input type="checkbox" class="form-check-input" value="" id="OtherCheck2">Evolved
                         </label>
                         <div class="clear"></div>
                     </div>
@@ -269,24 +266,19 @@
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OtherCheck8">Damage % increase
+                            <input type="checkbox" class="form-check-input" value="" id="OtherCheck6">Damage % increase
                         </label>
                         <div class="clear"></div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" value="" id="OtherCheck9">Proc on ability
+                            <input type="checkbox" class="form-check-input" value="" id="OtherCheck7">Proc on ability
                         </label>
                         <div class="clear"></div>
                     </div>
 
                 </div>
             </div>
-
-
-            <!-- </div>
-
-        <div class="col-sm-1"></div> -->
         </div>
     </div>
 </div>
